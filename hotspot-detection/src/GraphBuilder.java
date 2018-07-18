e38efbb16508b01c9454ca1fa3ae1b2fcee24e11
porke <porke@gazeta.pl>
2018-07-19 00:27:14 +0200

    Setup basic intellij project with the graph

---
+++ hotspot-detection/src/GraphBuilder.java
@@ -0,0 +1,28 @@
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+public class GraphBuilder {
+    public Graph<String, DefaultEdge> buildGraph(String input) {
+        Graph<String, DefaultEdge> g = new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
+
+        String v1 = "v1";
+        String v2 = "v2";
+        String v3 = "v3";
+        String v4 = "v4";
+
+        // add the vertices
+        g.addVertex(v1);
+        g.addVertex(v2);
+        g.addVertex(v3);
+        g.addVertex(v4);
+
+        // add edges to create a circuit
+        g.addEdge(v1, v2);
+        g.addEdge(v2, v3);
+        g.addEdge(v3, v4);
+        g.addEdge(v4, v1);
+
+        return g;
+    }
+}
---
+++ hotspot-detection/test/GraphBuilderTest.java
@@ -0,0 +1,19 @@
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+
+import static org.junit.Assert.*;
+
+public class GraphBuilderTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void buildGraph() {
+        GraphBuilder gb = new GraphBuilder();
+
+        Graph<String, DefaultEdge> stringDefaultEdgeGraph = gb.buildGraph("nanana!");
+        assertEquals(4, stringDefaultEdgeGraph.vertexSet().size());
+    }
+}
